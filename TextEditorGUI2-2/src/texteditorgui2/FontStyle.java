db6500726ca2e3a9cb423ae6fb4227b41597225e
zahraqureshi03 <147425090+zahraqureshi03@users.noreply.github.com>
2023-10-30 12:23:13 -0400

    GUI Update

---
+++ TextEditorGUI2-2/src/texteditorgui2/FontStyle.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package texteditorgui2;
+
+/**
+ *
+ * @author dylanmora
+ */
+public class FontStyle extends TextEditorGUI {
+    private String fontName;
+    private int fontStyle;
+    private int fontSize;
+
+    public FontStyle(String fontName, int fontStyle, int fontSize) {
+        this.fontName = fontName;
+        this.fontStyle = fontStyle;
+        this.fontSize = fontSize;
+    }
+    
+    public String getFontName(){
+    return "";
+    }
+    
+    public int fontStyle(){
+    return 0;
+    }
+    
+    public int fontSize(){
+    return 0;
+    }
+    
+    
+    
+}
